ed7d9c343816ca964e4c0da8b35b3787890bd0ac
George Gore <georgelgore@gmail.com>
2017-10-16 21:39:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -13,52 +13,10 @@ def consolidate_cart(cart)
 
   merged_hash
 end
-#   count = 0
-#   hash1 = Hash.new(0)
-#   final_hash = {}
-#
-#   cart.each do |items|
-#     items.each do |name, info_hash|
-#       hash1[name] += 1
-#     end
-#   end
-#
-#   new_cart = cart.uniq!
-#   new_cart.each do |item|
-#     item.each do |name, info_hash|
-#         new_info = {:count => hash1[name]}
-#        info_hash.merge!(new_info)
-#        final_hash.merge!(item)
-#    end
-# end
-#
-# new_cart
-#
-# end
 
-  # code here
-  # count = {:count => 0}
-  #
-  # hash = Hash.new(0)
-  # output_hash = {}
-  #
-  # cart.each do |item|
-  #   hash[item] += 1
-  # end
-  #
-  # hash.each do |hash1|
-  #   hash1.each do |hash2,count|
-  #     hash2.keys[:count] = count
-  #   end
-  # end
-  # cart.each do |item, item_info|
-  #   item_info <<
-  # end
-#   hash
-#
-# end
 
 def apply_coupons(cart, coupons)
+  puts coupons
   # code here
 end
 
