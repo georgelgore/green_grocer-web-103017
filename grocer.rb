e0e8fc8c59d7335d2221954d56410d5e228c012b
George Gore <georgelgore@gmail.com>
2017-10-17 01:24:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -32,7 +32,7 @@ def apply_clearance(cart)
   # code here
   cart.each do |hash|
     if hash.last[:clearance] == true
-      hash.last[:price] = hash.last[:price] *= (20/100)
+      p hash.last[:price] 
     end
   end
 end
